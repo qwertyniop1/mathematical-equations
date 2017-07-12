6e745a38bc3b8e34a559c2bcc8ea909d35aadace
qwertyniop1 <svitas1997@gmail.com>
2017-07-12 15:33:32 +0300

    frontend-server: Equations pages and controller
    Add routes for equations, add names for all routes. Linear and
    quadratic equations pages.
    Add Google fonts and favicons.

---
+++ equations-frontend/app/controllers/equations_controller.rb
@@ -0,0 +1,7 @@
+class EquationsController < ApplicationController
+  def linear
+  end
+
+  def quadratic
+  end
+end
--- equations-frontend/config/routes.rb
+++ equations-frontend/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'static#home'
 
-  get 'about', to: 'static#about'
-  get 'developers', to: 'static#api'
+  get 'about', to: 'static#about', as: :about
+  get 'developers', to: 'static#api', as: :api
+
+  scope 'equations', controller: :equations do
+    get 'linear', as: :linear
+    get 'quadratic', as: :quadratic
+  end
 end

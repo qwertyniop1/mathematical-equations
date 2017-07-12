ab12aa12c946293dfe0f30f022944ba5a4a12718
qwertyniop1 <svitas1997@gmail.com>
2017-07-12 13:50:36 +0300

    frontend-server: Static pages
    Create rails controller for displaying static pages. Add slim gem.
    Create application layout and templates for home, about, api pages.

--- equations-frontend/config/routes.rb
+++ equations-frontend/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static#home'
+
+  get 'about', to: 'static#about'
+  get 'api', to: 'static#api'
 end

8ce4b7b41bc582a9e7c417b19a72bca2d094513d
qwertyniop1 <svitas1997@gmail.com>
2017-07-13 13:55:38 +0300

    frontend-server: Behavior tests for application
    Install rspec and guard gems. Write behavior tests for static and
    equations pages.

--- equations-frontend/app/helpers/application_helper.rb
+++ equations-frontend/app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = 'Equations solver'
+    base_title = 'Equations Solver'
     if page_title.empty?
       base_title
     else
---
+++ equations-frontend/spec/requests/static_pages_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe 'StaticPages', type: :request do
+
+  subject { page }
+
+	shared_examples_for 'all static pages' do
+		it { should have_selector('li.active', text: heading) }
+		it { should have_title(full_title page_title) }
+	end
+
+  describe 'Home Page' do
+  	before { visit root_path}
+  	let(:heading) { 'Equations Solver' }
+  	let(:page_title) { '' }
+
+    it { should_not have_title ' | Home' }
+		it_should_behave_like 'all static pages'
+  end
+
+  describe 'About page' do
+		before { visit about_path}
+		let(:heading) { 'About' }
+  	let(:page_title) { 'About' }
+
+    it { should have_selector 'p', text: 'Contacts' }
+		it_should_behave_like 'all static pages'
+  end
+
+  describe 'Api page' do
+		before { visit api_path}
+    let(:heading) { 'Api' }
+  	let(:page_title) { 'Developers' }
+
+    it { should have_selector 'table' }
+		it_should_behave_like 'all static pages'
+  end
+
+  it 'should have the right links on the layout' do
+    visit root_path
+    click_link 'Equations Solver'
+  	expect(page).to have_title(full_title '')
+  	click_link 'About'
+  	expect(page).to have_title(full_title 'About')
+  	click_link 'Api'
+  	expect(page).to have_title(full_title 'Developers')
+  	click_link 'Linear'
+    expect(page).to have_title(full_title 'Linear equations')
+    click_link 'Quadratic'
+    expect(page).to have_title(full_title 'Quadratic equations')
+    click_link 'Sign up'
+  	expect(page).to have_title(full_title 'Sign up')
+    click_link 'Sign in'
+    expect(page).to have_title(full_title 'Sign in')
+  end
+end

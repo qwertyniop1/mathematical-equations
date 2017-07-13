5e054ddac34339548fd899c7de3703d61b5b373a
qwertyniop1 <svitas1997@gmail.com>
2017-07-13 16:20:26 +0300

    api-integration: Error messages animation
    Add CSS animation for errors popup and fade out. Refactor JS code.

--- equations-frontend/app/assets/javascripts/equation_form.js
+++ equations-frontend/app/assets/javascripts/equation_form.js
@@ -1,44 +1,3 @@
-document.addEventListener('turbolinks:load', () => {
-    const expression = document.querySelector('.equation__input');
-    const inputs = document.querySelectorAll('.form input[type=number]');
-    const form = document.querySelector('.form');
-
-    inputs.forEach(input => input.addEventListener('change', (event) => {
-        expression.innerHTML = getNewExpression(inputs);
-    }));
-
-    inputs.forEach(input => input.addEventListener('input', (event) => {
-        validate(event.target);
-    }));
-
-    if (form) {
-        form.querySelector('input[type=submit').addEventListener('click', (event) => {
-            const coefficient_a = document.getElementById('coefficient_a');
-            const errors = document.querySelector('.equation__error');
-
-            if (coefficient_a.value === '0') {
-                event.preventDefault();
-                coefficient_a.classList.add('error');
-                errors.innerHTML = 'Coefficient A cannot equals to 0.'
-                errors.classList.remove('hidden');
-            } else {
-                coefficient_a.classList.remove('error');
-                errors.classList.add('hidden');
-            }
-        });
-    }
-
-    expression.innerHTML = getNewExpression(inputs);
-});
-
-document.addEventListener('ajax:send', (event, xhr, options) => {
-    document.querySelector('.fade').classList.remove('hidden');
-});
-
-document.addEventListener('ajax:complete', (event, xhr, status) => {
-    document.querySelector('.fade').classList.add('hidden');
-});
-
 const getNewExpression = (inputs) => {
     const [a, b, c] = [].map.call(inputs, input => input.value || 0);
     if (inputs.length === 3) {
@@ -55,3 +14,68 @@ const validate = (input) => {
         input.value = value.slice(0, -1);
     }
 };
+
+const errors = {
+    init(selector) {
+        this.element = document.querySelector(selector);
+    },
+    show (message) {
+        this.element.innerHTML = message;
+        this.element.classList.add('visible');
+
+        const self = this;
+        setTimeout(() => {
+            self.hide();
+        }, 5000);
+    },
+    hide() {
+        this.element.classList.remove('visible');
+    },
+};
+
+
+document.addEventListener('turbolinks:load', () => {
+    const expression = document.querySelector('.equation__input');
+    const inputs = document.querySelectorAll('.form input[type=number]');
+    const form = document.querySelector('.form');
+
+    inputs.forEach(input => input.addEventListener('change', (event) => {
+        expression.innerHTML = getNewExpression(inputs);
+    }));
+
+    inputs.forEach(input => input.addEventListener('input', (event) => {
+        validate(event.target);
+    }));
+
+    errors.init('.equation__error');
+
+    if (form) {
+        form.querySelector('input[type=submit').addEventListener('click', (event) => {
+            const coefficient_a = document.getElementById('coefficient_a');
+
+            if (coefficient_a.value === '0') {
+                event.preventDefault();
+                coefficient_a.classList.add('error');
+                errors.show('Coefficient A cannot equals to 0.');
+
+            } else {
+                coefficient_a.classList.remove('error');
+                errors.hide();
+            }
+        });
+
+        expression.innerHTML = getNewExpression(inputs);
+    }
+});
+
+document.addEventListener('ajax:send', (event, xhr, options) => {
+    document.querySelector('.fade').classList.remove('hidden');
+});
+
+document.addEventListener('ajax:complete', (event, xhr, status) => {
+    document.querySelector('.fade').classList.add('hidden');
+});
+
+document.addEventListener('ajax:error', (event, xhr, status) => {
+    errors.show('Server error occured. Please, try again later.')
+});
